42fa19dff90d10399c7c57fd2e3dd777c44ed2f5
Hernán Navarro <51421389+hjnsxx@users.noreply.github.com>
2022-12-01 09:29:29 -0400

    Delete PrinTax Account 13, 14, 15/printax_account_module directory

--- PrinTax Account 13, 14, 15/printax_account_module/models/res_currency.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models
-
-class ResCurrency(models.Model):
-    _inherit = 'res.currency'
-
-    printax_idp = fields.Char(string='Código fiscal del instrumento de pago para PrinTax', default='01', size = 2, config_parameter='printax.idp')
-
